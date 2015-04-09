50c506375fb783f0210ca5c8b694a49988c0f2a8
Artur Opala <artur.opala@siili.fi>
2015-04-09 17:02:12 +0200

    defining settings

--- project/Build.scala
+++ project/Build.scala
@@ -1,7 +1,18 @@
 import sbt._
 import Keys._
 
+object CommonSettings {
+	val scalaVersion = "2.11.6"
+}
+
 object Release extends Build {
 
+	val versionSetting:Setting[String] = Keys.version in Global := "0.1.a"
+
 	val main = Project("main", file("."))
+		.settings(
+   			versionSetting,
+   			organization := "fi.siili",
+    		scalaVersion := CommonSettings.scalaVersion
+		)
 }
