80314fbc583a15d427319721b3515452a9bff4bc
Artur Opala <artur.opala@siili.fi>
2015-04-10 12:55:46 +0200

    adding task and plugin

--- project/Build.scala
+++ project/Build.scala
@@ -5,14 +5,28 @@ object CommonSettings {
 	val scalaVersion = "2.11.6"
 }
 
-object Release extends Build {
-
-	val versionSetting:Setting[String] = Keys.version in Global := "0.1.a"
+object MyBuild extends Build {
 
 	val main = Project("main", file("."))
 		.settings(
-   			versionSetting,
+   			version := "0.1.a",
    			organization := "fi.siili",
-    		scalaVersion := CommonSettings.scalaVersion
+    		scalaVersion := CommonSettings.scalaVersion,
+    		MyPlugin.dumbSetting := "Scala source: "+(scalaSource in Compile).value.toString
 		)
+		.enablePlugins(MyPlugin)
+}
+
+object MyPlugin extends AutoPlugin {
+
+    val dumbSetting = settingKey[String]("A new setting 01.")
+	val dumbTask = taskKey[Unit]("A new task 01.")
+
+	def dumbSettings = Seq(
+		dumbTask := println(dumbSetting.value),
+		(compile in Compile) <<= (compile in Compile).dependsOn(dumbTask)
+	)
+
+	override def projectSettings: Seq[Setting[_]] = dumbSettings
+
 }
